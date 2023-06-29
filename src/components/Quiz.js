1037286bf13b386dcdd07f4546420ecabfe85fb6
mohamedmehdigara <garamehdi57@yahoo.com>
2023-06-29 17:04:00 +0100

    added files

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -2,17 +2,42 @@ import React, { useState } from 'react';
 
 const Quiz = () => {
   const [questionIndex, setQuestionIndex] = useState(0);
+  const [showMessage, setShowMessage] = useState(false);
+  const [message, setMessage] = useState('');
   const questions = [
-    // Replace this with your actual array of question objects
-    { question: 'Approximately how many people are on Earth?' },
-    { question: 'How many continents are on Earth?' },
-    { question: 'Which country has Tokyo as its capital?' },
+    {
+      question: 'How many people are on Earth?',
+      choices: ['5 billions', '6 billions', '7 billions'],
+      correctAnswer: '7 billions',
+    },
+    {
+      question: 'How many continents are there?',
+      choices: ['3', '4', '7'],
+      correctAnswer: '7',
+    },
+    {
+      question: 'Which country has Tokyo as its capital?',
+      choices: ['South Korea', 'Japan', 'Taiwan'],
+      correctAnswer: 'Japan',
+    },
   ];
 
   const currentQuestion = questions[questionIndex];
 
   const handleNextQuestion = () => {
     setQuestionIndex(questionIndex + 1);
+    setShowMessage(false);
+    setMessage('');
+  };
+
+  const handleAnswer = (selectedAnswer) => {
+    if (selectedAnswer === currentQuestion.correctAnswer) {
+      setShowMessage(true);
+      setMessage('Correct!');
+    } else {
+      setShowMessage(true);
+      setMessage('Incorrect!');
+    }
   };
 
   return (
@@ -21,7 +46,15 @@ const Quiz = () => {
         <div>
           <h2>Question:</h2>
           <p>{currentQuestion.question}</p>
+          <ul>
+            {currentQuestion.choices.map((choice, index) => (
+              <li key={index}>
+                <button onClick={() => handleAnswer(choice)}>{choice}</button>
+              </li>
+            ))}
+          </ul>
           <button onClick={handleNextQuestion}>Next Question</button>
+          {showMessage && <p>{message}</p>}
         </div>
       )}
     </div>
