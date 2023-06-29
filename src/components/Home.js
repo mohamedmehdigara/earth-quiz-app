de599686a3f68d84892206d477bf27dff092406f
mohamedmehdigara <garamehdi57@yahoo.com>
2023-06-29 15:42:16 +0100

    added files

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ResponsiveAppBar from "../ResponsiveAppBar";
+import ResponsiveAppBar from "./ResponsiveAppBar";
 import {useNavigate} from "react-router-dom";
 import Grid from '@mui/material/Grid';
 import Typography from "@mui/material/Typography";
